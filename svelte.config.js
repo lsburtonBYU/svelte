a9bc44c2b3ecc78dc838c0a84c1fa7c5b36f8eb2
Lara Burton <lsburton@byu.edu>
2022-08-08 15:19:54 -0600

    fix svelte.config base path node env var

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-// const dev = process.env.NODE_ENV === 'development';
-const base = '/svelte';
+const base = process.env['NODE_ENV'] === 'production' ? '/svelte' : '';
+// const base = import.meta.env.DEV ? '' : '/svelte';
+// const base = '/svelte';
 // const base = '';
 
 export default {
 	kit: {
-		paths: {
-			base: base
-		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
@@ -17,6 +15,10 @@ export default {
 			fallback: null,
 			precompress: false
 		}),
+
+		paths: {
+			base: base
+		},
 
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
